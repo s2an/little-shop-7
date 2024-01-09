115f0d821f6e00beaf8c115c2bc6d168ce8b49d8
Dylan Perry <dylanbperry@gmail.com>
2024-01-08 18:31:35 -0800

    Reformat top_five_customers to be more readable

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,6 +7,11 @@ class Merchant < ApplicationRecord
   end
 
   def top_five_customers
-    Customer.joins(invoices: [{ items: :merchant }, :transactions] ).select("customers.*, count(customers.id) as number_of_transactions").where("transactions.result = 1 AND merchant_id = #{self.id}").order("COUNT(customers.id) DESC").group(:id).limit(5)
+    Customer.joins(invoices: [{ items: :merchant }, :transactions] )
+            .select("customers.*, count(customers.id) as number_of_transactions")
+            .where("transactions.result = 1 AND merchant_id = #{self.id}")
+            .order("COUNT(customers.id) DESC")
+            .group(:id)
+            .limit(5)
   end
 end
