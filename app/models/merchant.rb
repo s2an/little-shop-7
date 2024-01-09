2fb7ed97730c78fe39bce25f6ab88fbf860a93fe
Dylan Perry <dylanbperry@gmail.com>
2024-01-08 18:14:32 -0800

    Add merchant instance method and tests top_five_customers for user story 3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,7 @@ class Merchant < ApplicationRecord
     Item.where(merchant_id: id).pluck(:id)
   end
 
+  def top_five_customers
+    Customer.joins(invoices: [{ items: :merchant }, :transactions] ).select("customers.*, count(customers.id) as number_of_transactions").where("transactions.result = 1 AND merchant_id = #{self.id}").order("COUNT(customers.id) DESC").group(:id).limit(5)
+  end
 end
